1f620a693f118b620189446c6d6bbd02bd1d682c
eachen <stopratracingplz@gmail.com>
2025-02-28 15:30:49 +0800

    update

---
+++ examples/examples_2D/04_rayleigh_taylor_instability/run.py
@@ -0,0 +1,45 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+from jaxfluids import InputManager, InitializationManager, SimulationManager
+from jaxfluids_postprocess import load_data, create_2D_animation, create_2D_figure
+
+# SETUP SIMULATION
+input_manager = InputManager("rti.json", "numerical_setup.json")
+domain_information = input_manager.domain_information
+initialization_manager = InitializationManager(input_manager)
+sim_manager = SimulationManager(input_manager)
+
+# RUN SIMULATION
+simulation_buffers, time_control_variables, \
+forcing_parameters = initialization_manager.initialization()
+sim_manager.simulate(simulation_buffers, time_control_variables, forcing_parameters)
+
+# LOAD DATA
+path = sim_manager.output_writer.save_path_domain
+quantities = ["density", "velocity", "pressure", "vorticity"]
+cell_centers, cell_sizes, times, data_dict = load_data(path, quantities)
+
+# PLOT
+nrows_ncols = (1,2)
+plot_dict = {
+    "density": data_dict["density"], 
+    "vorticity": data_dict["vorticity"][:,2]
+}
+
+# CREATE ANIMATION
+create_2D_animation(
+    plot_dict,
+    cell_centers,
+    times,
+    nrows_ncols=nrows_ncols,
+    plane="xy",
+    interval=200)
+
+# CREATE FIGURE
+create_2D_figure(
+    plot_dict, 
+    nrows_ncols, 
+    cell_centers=cell_centers,
+    plane="xy", plane_value=0.0, 
+    save_fig="rti.png")
---
+++ src/jaxfluids/data_types/case_setup/general.py
@@ -0,0 +1,13 @@
+from typing import NamedTuple
+import numpy as np
+
+class GeneralSetup(NamedTuple):
+    case_name: str
+    end_time: float
+    end_step: int
+    save_path: str
+    save_dt: float
+    save_step: int
+    save_timestamps: np.ndarray
+    save_start: float
+    convergence_tol: float = 0.
